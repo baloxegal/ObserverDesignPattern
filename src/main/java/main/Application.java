ddb5d0f002e1180889cd636792b6e80244f45c44
vbalan <vbalan@patriot.ambianta.local>
2020-11-19 16:58:18 +0200

    final for monday

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -2,37 +2,35 @@ package main;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import com.twelvemonkeys.image.ResampleOp;
+import java.io.IOException;
 
 public class Application {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
-		String pathOriginal = "images/original";
-		String pathRefactor = "images/refactor";
+		String pathOriginal = "images/original/";
+		String pathRefactor = "images/refactor/";
 				
 		File directoryOriginal = new File (pathOriginal);
 		File directoryRefactor = new File (pathRefactor);
 		
 		List <String> extensions = new ArrayList<>(Arrays.asList(".jpg", ".jpeg", ".png"));
 		
-		if(!directoryOriginal.exists()) {
-			
-			System.out.println("Directory - \"" + pathOriginal + "\" not found");
-			
+		if(!directoryOriginal.exists()) {			
+			System.out.println("Directory - \"" + pathOriginal + "\" not found");			
 			return;
 		}
 		
-		if(!directoryRefactor.exists()) {
-			
-			System.out.println("Directory - \"" + pathRefactor + "\" not found");
-			
+		if(!directoryRefactor.exists()) {			
+			System.out.println("Directory - \"" + pathRefactor + "\" not found");			
 			return;
 		}
 		
@@ -40,7 +38,6 @@ public class Application {
 			
 			@Override
 			public boolean accept(File pathname) {
-			
 				if(pathname.isFile() && extensions.contains(pathname.getName().substring(pathname.getName().lastIndexOf(".")).toLowerCase()))
 					return true;
 				
@@ -52,7 +49,6 @@ public class Application {
 			
 			@Override
 			public boolean accept(File pathname) {
-			
 				if(pathname.isFile() && extensions.contains(pathname.getName().substring(pathname.getName().lastIndexOf(".")).toLowerCase()))
 					return true;
 				
@@ -61,13 +57,10 @@ public class Application {
 		});;	
 		
 		if(lastImages.length == 0 && refactorImages.length != 0) {
-		
 			for(var f : refactorImages) {
 				f.delete();
-			}
-				
-			Arrays.asList(refactorImages).clear();
-			
+			}				
+			Arrays.asList(refactorImages).clear();			
 		}
 		
 		if(lastImages.length != 0) {
@@ -78,32 +71,38 @@ public class Application {
 			System.out.println("Directory - \"" + pathOriginal + "\" contains");
 			System.out.println("FILES LIST:");
 			
-			for(var f_1 : lastImages) {
-														
-				System.out.println(">>> " + f_1);
-				
+			for(var f : lastImages) {														
+				System.out.println(">>> " + f);				
 			}
 						
 			if(!Arrays.stream(refactorImages).map(r -> r.getName()).collect(Collectors.toList()).equals(Arrays.stream(lastImages).map(l -> l.getName()).collect(Collectors.toList()))) {
 				
 				for(var f : lastImages) {
-					if(!Arrays.stream(refactorImages).map(rf -> rf.getName()).collect(Collectors.toList()).contains(f.getName())) {
-						
+					if(!Arrays.stream(refactorImages).map(rf -> rf.getName()).collect(Collectors.toList()).contains(f.getName())) {						
 						BufferedImage image = ImageIO.read(f);
-						ImageIO.write(image, "jpeg", new File (pathRefactor + "/" + f.getName()));						
+						BufferedImageOp resampler = new ResampleOp(100, 100, ResampleOp.FILTER_LANCZOS);
+						BufferedImage resampledImage = resampler.filter(image, null);
+						ImageIO.write(resampledImage, "jpeg", new File (pathRefactor + f.getName()));						
 					}
 				}
 								
 				for(var f : refactorImages) {
 					if(!Arrays.stream(lastImages).map(lf -> lf.getName()).collect(Collectors.toList()).contains(f.getName())) {
-						
-						f.delete();
-					
+						f.delete();						
 					}
 				}
-								
-			}
-			
+				
+				refactorImages = directoryRefactor.listFiles(new FileFilter() {
+					
+					@Override
+					public boolean accept(File pathname) {
+						if(pathname.isFile() && extensions.contains(pathname.getName().substring(pathname.getName().lastIndexOf(".")).toLowerCase()))
+							return true;
+						
+						return false;
+					}
+				});;				
+			}			
 		}
 				
 		File[] currentImages = {};
@@ -117,7 +116,6 @@ public class Application {
 				
 				@Override
 				public boolean accept(File pathname) {
-
 					if(pathname.isFile() && extensions.contains(pathname.getName().substring(pathname.getName().lastIndexOf(".")).toLowerCase()))
 						return true;
 					
@@ -128,91 +126,96 @@ public class Application {
 			if(currentImages.length != 0)
 				Arrays.sort(currentImages);
 								
-			if(!Arrays.equals(currentImages, lastImages)) {
-								
+			if(!Arrays.equals(currentImages, lastImages)) {								
 				System.out.println("Directory - \"" + pathOriginal + "\" has been changed");
 					
-				if(currentImages.length == 0) {
-					
+				if(currentImages.length == 0) {					
 					System.out.println("Directory - \"" + pathOriginal + "\" is empty now, all files have been deleted");
-					lastImages = currentImages;
+					for(var f : refactorImages)
+						f.delete();
+					refactorImages = lastImages = currentImages;					
 					continue;
 				}
 					
 				System.out.println("FILES LIST:");
 					
 				for(var f : currentImages) {
-						
 					if(!Arrays.asList(lastImages).contains(f)) {
 						differenceAddImages.add(f);
 					}
-																			
 					System.out.println(">>> " + f);
-						
 				}
 				
-				for(var f : lastImages) {
-					
+				for(var f : lastImages) {					
 					if(!Arrays.asList(currentImages).contains(f)) {
 						differenceDeleteImages.add(f);
 					}
 				}
 				
-				if(!differenceAddImages.isEmpty()) {
-					
-					System.out.println("To the directory - \"" + pathOriginal + "\" has been added files: ");
-					
+				if(!differenceAddImages.isEmpty()) {					
+					System.out.println("To the directory - \"" + pathOriginal + "\" has been added files: ");					
 					for(var f : differenceAddImages) {
-						
+						System.out.println(">>> " + f);
 						BufferedImage image = ImageIO.read(f);
-						ImageIO.write(image, "jpeg", new File (pathRefactor + "/" + f.getName()));						
-						
-						System.out.println(">>> " + f);
-							
-					}
-					
+						BufferedImageOp resampler = new ResampleOp(100, 100, ResampleOp.FILTER_LANCZOS);
+						BufferedImage resampledImage = resampler.filter(image, null);
+						ImageIO.write(resampledImage, "jpeg", new File (pathRefactor + f.getName()));																
+					}					
 				}
 				
-				if(!differenceDeleteImages.isEmpty()) {
-					
-					System.out.println("From the directory - \"" + pathOriginal + "\" has been deleted files: ");
-					
+				if(!differenceDeleteImages.isEmpty()) {					
+					System.out.println("From the directory - \"" + pathOriginal + "\" has been deleted files: ");					
 					for(var f : differenceDeleteImages) {
-						
-						new File (pathRefactor + "/" + f.getName()).delete();
-						
 						System.out.println(">>> " + f);
-							
-					}
-									
+						new File (pathRefactor + f.getName()).delete();												
+					}									
 				}
-				
+												
 				lastImages = currentImages;
 				differenceAddImages.clear();
-				differenceDeleteImages.clear();
+				differenceDeleteImages.clear();				
+			}
+			
+			refactorImages = directoryRefactor.listFiles(new FileFilter() {
 				
+				@Override
+				public boolean accept(File pathname) {
+					if(pathname.isFile() && extensions.contains(pathname.getName().substring(pathname.getName().lastIndexOf(".")).toLowerCase()))
+						return true;
+					
+					return false;
+				}
+			});;	
+			
+			if(!Arrays.stream(refactorImages).map(r -> r.getName()).collect(Collectors.toList()).equals(Arrays.stream(currentImages).map(l -> l.getName()).collect(Collectors.toList()))) {
+				
+				for(var f : currentImages) {
+					if(!Arrays.stream(refactorImages).map(rf -> rf.getName()).collect(Collectors.toList()).contains(f.getName())) {						
+						BufferedImage image = ImageIO.read(f);
+						BufferedImageOp resampler = new ResampleOp(100, 100, ResampleOp.FILTER_LANCZOS);
+						BufferedImage resampledImage = resampler.filter(image, null);
+						ImageIO.write(resampledImage, "jpeg", new File (pathRefactor + f.getName()));							
+					}
+				}
+								
+				for(var f : refactorImages) {
+					if(!Arrays.stream(currentImages).map(lf -> lf.getName()).collect(Collectors.toList()).contains(f.getName())) {
+						f.delete();						
+					}
+				}
+								
 				refactorImages = directoryRefactor.listFiles(new FileFilter() {
 					
 					@Override
 					public boolean accept(File pathname) {
-					
 						if(pathname.isFile() && extensions.contains(pathname.getName().substring(pathname.getName().lastIndexOf(".")).toLowerCase()))
 							return true;
 						
 						return false;
 					}
-				});;	
-				
-			}
-						
-			Thread.sleep(10000);
-			
-		}		
-
+				});;				
+			}			
+			Thread.sleep(10000);			
+		}
 	}
-	
-//	public static void transformerProcessor(String name) {
-//		BufferedImage image = ImageIO.read(new File())
-//	}
-
 }
